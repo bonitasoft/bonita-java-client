1bb8df854822e37bb0f06f0b33d04fd1f0952c09
hugues.bretin <hugues.bretin@bonitasoft.com>
2020-11-12 16:38:51 +0100

    ci(sonar): add sonar analysis

--- src/main/java/org/bonitasoft/web/client/BonitaClient.java
+++ src/main/java/org/bonitasoft/web/client/BonitaClient.java
@@ -4,18 +4,45 @@ import org.bonitasoft.web.client.feign.BonitaFeignClientBuilderImpl;
 import org.bonitasoft.web.client.model.Session;
 import org.bonitasoft.web.client.services.*;
 
+/**
+ * A Bonita client that communicate via http request with a Bonita running instance.
+ */
 public interface BonitaClient {
 
     String DEFAULT_TENANT_ID = "1";
     String USER_PROFILE_NAME = "User";
     String ADMIN_PROFILE_NAME = "Administrator";
 
+    /**
+     * Create a client builder for the given bonita URL
+     *
+     * @param url the bonita instance url
+     * @return a builder instance
+     */
     static BonitaClientBuilder<?> builder(String url) {
         return new BonitaFeignClientBuilderImpl(url);
     }
 
+    /**
+     * Perform client authentication. Authentication will be stored internally to be used for the future requests <br>
+     * Account tenant default to 1
+     * @param username Account username
+     * @param password Account password
+     * @return A session with the authenticated user info
+     * @throws org.bonitasoft.web.client.exception.UnauthorizedException if authentication failed
+     */
     Session login(String username, String password);
 
+    /**
+     /**
+     * Perform client authentication. Authentication will be stored internally to be used for the future requests
+     *
+     * @param username Account username
+     * @param password Account password
+     * @throws org.bonitasoft.web.client.exception.UnauthorizedException if authentication failed
+     * @param tenant Account tenant
+     * @return A session with the authenticated user info
+     */
     Session login(String username, String password, String tenant);
 
     void logout();
