8084e14551a514ab7638b10ffe9e3df673437ab4
hugues.bretin <hugues.bretin@bonitasoft.com>
2020-11-13 14:27:39 +0100

    ci(qa): sonar fix

--- src/main/java/org/bonitasoft/web/client/invoker/auth/BonitaLoginService.java
+++ src/main/java/org/bonitasoft/web/client/invoker/auth/BonitaLoginService.java
@@ -10,8 +10,6 @@ package org.bonitasoft.web.client.invoker.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
-import java.io.IOException;
-import java.io.InputStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bonitasoft.web.client.api.PortalAuthenticationApi;
@@ -22,56 +20,67 @@ import org.bonitasoft.web.client.feign.ApiProvider;
 import org.bonitasoft.web.client.model.Session;
 import org.bonitasoft.web.client.services.LoginService;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import static java.lang.String.*;
+
 @Slf4j
 @RequiredArgsConstructor
 public class BonitaLoginService implements LoginService {
 
-  private final ApiProvider apiProvider;
-  private final ObjectMapper objectMapper;
-  private final BonitaCookieAuth bonitaCookieAuth;
+    private final ApiProvider apiProvider;
+    private final ObjectMapper objectMapper;
+    private final BonitaCookieAuth bonitaCookieAuth;
 
-  @Override
-  public Session login(String username, String password, String tenant) {
-    log.debug("Login with user '{}' on tenant '{}'...", username, tenant);
-    try (Response loginResponse =
-        apiProvider
-            .get(PortalAuthenticationApi.class)
-            .login(username, password, tenant, "false", "")) {
-      if (loginResponse.status() == 200 || loginResponse.status() == 204) {
-        bonitaCookieAuth.setSessionCookies(loginResponse.headers());
-      } else {
-        throw new UnauthorizedException(
-            String.format(
-                "Login failed, status: %S %s", loginResponse.status(), loginResponse.reason()));
-      }
-    } catch (UnauthorizedException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new ClientException("Login failed", e);
+    @Override
+    public Session login(String username, String password, String tenant) {
+        log.debug("Login with user '{}' on tenant '{}'...", username, tenant);
+
+        boolean loginSucceeded;
+        int loginStatus;
+        String loginReason = "";
+
+        final PortalAuthenticationApi portalAuthenticationApi = apiProvider.get(PortalAuthenticationApi.class);
+        try (Response loginResponse = portalAuthenticationApi.login(username, password, tenant, "false", "")) {
+            loginStatus = loginResponse.status();
+            loginSucceeded = (loginStatus == 200 || loginStatus == 204);
+            if (loginSucceeded) {
+                bonitaCookieAuth.setSessionCookies(loginResponse.headers());
+            } else {
+                loginReason = loginResponse.reason();
+            }
+        } catch (Exception e) {
+            throw new ClientException("Login failed", e);
+        }
+
+        if (!loginSucceeded) {
+            throw new UnauthorizedException(
+                    format( "Login failed, status: %s %s", loginStatus, loginReason));
+        }
+
+        // check the session is ok + it will trigger the loading of servlets
+        Session session = getSession();
+        log.debug("Login completed. Session: {}", session);
+        return session;
     }
 
-    // check the session is ok + it will trigger the loading of servlets
-    Session session = getSession();
-    log.debug("Login completed. Session: {}", session);
-    return session;
-  }
-
-  @Override
-  public Session getSession() {
-    Response response = apiProvider.get(SessionApi.class).getSession();
-    try (final Response.Body body = response.body()) {
-      InputStream inputStream = body.asInputStream();
-      return objectMapper.readValue(inputStream, Session.class);
-    } catch (IOException e) {
-      throw new ClientException("Failed to parse response as a Session:", e);
+    @Override
+    public Session getSession() {
+        Response response = apiProvider.get(SessionApi.class).getSession();
+        try (final Response.Body body = response.body()) {
+            InputStream inputStream = body.asInputStream();
+            return objectMapper.readValue(inputStream, Session.class);
+        } catch (IOException e) {
+            throw new ClientException("Failed to parse response as a Session:", e);
+        }
     }
-  }
 
-  @Override
-  public void logout() {
-    log.debug("Logout...");
-    apiProvider.get(PortalAuthenticationApi.class).logout("false");
-    bonitaCookieAuth.clearSessionCookie();
-    log.debug("Logout completed.");
-  }
+    @Override
+    public void logout() {
+        log.debug("Logout...");
+        apiProvider.get(PortalAuthenticationApi.class).logout("false");
+        bonitaCookieAuth.clearSessionCookie();
+        log.debug("Logout completed.");
+    }
 }
