cbc0f739a971b4e40c65c3981439301c3d3530b6
hugues.bretin <hugues.bretin@bonitasoft.com>
2021-11-12 16:05:22 +0100

    chore(openapi): use 0.0.4 version of bonita openapi

--- src/main/java/org/bonitasoft/web/client/services/impl/DefaultBdmService.java
+++ src/main/java/org/bonitasoft/web/client/services/impl/DefaultBdmService.java
@@ -1,19 +1,26 @@
 package org.bonitasoft.web.client.services.impl;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import lombok.extern.slf4j.Slf4j;
 import org.bonitasoft.web.client.BonitaClient;
 import org.bonitasoft.web.client.api.BdmAccessControlApi;
 import org.bonitasoft.web.client.api.BdmApi;
+import org.bonitasoft.web.client.api.BusinessDataQueryApi;
+import org.bonitasoft.web.client.api.BusinessDataQueryApi.SearchBusinessDataQueryParams;
 import org.bonitasoft.web.client.api.SystemTenantApi;
 import org.bonitasoft.web.client.api.UploadApi;
+import org.bonitasoft.web.client.exception.ClientException;
 import org.bonitasoft.web.client.exception.LicenseException;
 import org.bonitasoft.web.client.feign.ApiProvider;
 import org.bonitasoft.web.client.model.BDMAccessControl;
 import org.bonitasoft.web.client.model.BDMInstallRequest;
 import org.bonitasoft.web.client.model.Bdm;
+import org.bonitasoft.web.client.model.BusinessData;
 import org.bonitasoft.web.client.model.TenantPauseRequest;
 import org.bonitasoft.web.client.model.TenantResourceState;
 import org.bonitasoft.web.client.services.BdmService;
@@ -23,85 +30,151 @@ import org.bonitasoft.web.client.services.impl.base.ClientContext;
 @Slf4j
 public class DefaultBdmService extends AbstractService implements BdmService {
 
-    public DefaultBdmService(
-            ClientContext clientContext, ApiProvider apiProvider, ObjectMapper objectMapper) {
-        super(apiProvider, objectMapper, clientContext);
-    }
+	public DefaultBdmService(
+			ClientContext clientContext, ApiProvider apiProvider, ObjectMapper objectMapper) {
+		super(apiProvider, objectMapper, clientContext);
+	}
 
-    @Override
-    public void importBDM(File bdm) {
-        log.info("Importing Business Data Model file: {}", bdm.getName());
+	@Override
+	public void importBDM(File bdm) {
+		log.info("Importing Business Data Model file: {}", bdm.getName());
 
-        // Pause tenant
-        log.debug("Pausing tenant ...");
-        SystemTenantApi tenantApi = apiProvider.get(SystemTenantApi.class);
-        tenantApi.updateSystemTenant(
-                BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("true"));
-        log.debug("Tenant paused");
+		// Pause tenant
+		log.debug("Pausing tenant ...");
+		SystemTenantApi tenantApi = apiProvider.get(SystemTenantApi.class);
+		tenantApi.updateSystemTenant(
+				BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("true"));
+		log.debug("Tenant paused");
 
-        deleteBdmAccessControlIfNeeded();
+		deleteBdmAccessControlIfNeeded();
 
-        UploadApi uploadApi = apiProvider.get(UploadApi.class);
-        String uploadedFileName = uploadApi.uploadFile(bdm);
-        log.debug("BDM file uploaded");
+		UploadApi uploadApi = apiProvider.get(UploadApi.class);
+		String uploadedFileName = uploadApi.uploadFile(bdm);
+		log.debug("BDM file uploaded");
 
 		BdmApi bdmApi = apiProvider.get(BdmApi.class);
 		bdmApi.installBDM(new BDMInstallRequest().fileUpload(uploadedFileName));
-        log.debug("BDM file installed");
+		log.debug("BDM file installed");
 
-        // Restart tenant
-        log.debug("Resuming tenant ...");
-        tenantApi.updateSystemTenant(
-                BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("false"));
-        log.debug("Tenant Resumed");
+		// Restart tenant
+		log.debug("Resuming tenant ...");
+		tenantApi.updateSystemTenant(
+				BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("false"));
+		log.debug("Tenant Resumed");
 
-        log.info("Business Data Model deployed successfully.");
-    }
+		log.info("Business Data Model deployed successfully.");
+	}
 
-    @Override
-    public void importBdmAccessControl(File accessControl) {
-        log.info("Importing BDM AccessControl file: {}", accessControl.getName());
+	@Override
+	public void importBdmAccessControl(File accessControl) {
+		log.info("Importing BDM AccessControl file: {}", accessControl.getName());
 
-        deleteBdmAccessControlIfNeeded();
+		deleteBdmAccessControlIfNeeded();
 
-        BdmAccessControlApi bdmAccessControlApi = apiProvider.get(BdmAccessControlApi.class);
-        String uploadedFileName = bdmAccessControlApi.uploadBDMAccessControl(accessControl);
-        log.debug("BDM Access Control file uploaded");
-        bdmAccessControlApi.importBDMAccessControl(uploadedFileName);
-        log.debug("BDM Access Control file installed");
+		BdmAccessControlApi bdmAccessControlApi = apiProvider.get(BdmAccessControlApi.class);
+		String uploadedFileName = bdmAccessControlApi.uploadBDMAccessControl(accessControl);
+		log.debug("BDM Access Control file uploaded");
+		bdmAccessControlApi.importBDMAccessControl(uploadedFileName);
+		log.debug("BDM Access Control file installed");
 
-        log.info("BDM AccessControl file imported successfully");
-    }
+		log.info("BDM AccessControl file imported successfully");
+	}
 
-    void deleteBdmAccessControlIfNeeded() {
-        if (!isCommunity()
-                && TenantResourceState.INSTALLED.equals(getBdmAccessControlStatus().getState())) {
-            log.debug("Deleting previous BdmAccessControl ...");
-            BdmAccessControlApi accessControlApi = apiProvider.get(BdmAccessControlApi.class);
-            accessControlApi.deleteBDMAccessControl();
-            log.debug("Previous BdmAccessControl deleted");
-        }
-    }
+	void deleteBdmAccessControlIfNeeded() {
+		if (!isCommunity()
+				&& TenantResourceState.INSTALLED.equals(getBdmAccessControlStatus().getState())) {
+			log.debug("Deleting previous BdmAccessControl ...");
+			BdmAccessControlApi accessControlApi = apiProvider.get(BdmAccessControlApi.class);
+			accessControlApi.deleteBDMAccessControl();
+			log.debug("Previous BdmAccessControl deleted");
+		}
+	}
 
-    @Override
-    public Bdm getBdmStatus() {
-        log.info("Get BDM status");
-        BdmApi bdmApi = apiProvider.get(BdmApi.class);
-        Bdm bdm = bdmApi.getBdm();
-        log.debug("BDM status: {}", bdm);
-        return bdm;
-    }
+	@Override
+	public Bdm getBdmStatus() {
+		log.info("Get BDM status");
+		BdmApi bdmApi = apiProvider.get(BdmApi.class);
+		Bdm bdm = bdmApi.getBdm();
+		log.debug("BDM status: {}", bdm);
+		return bdm;
+	}
 
-    @Override
-    public BDMAccessControl getBdmAccessControlStatus() {
-        log.info("Get BDM AccessControl status");
-        if (isCommunity()) {
-            String message = "BDM AccessControl management skipped: your current Bonita license is 'Community' and this feature is only supported in 'Subscription' editions.";
-            throw new LicenseException(message);
-        }
-        BdmAccessControlApi accessControlApi = apiProvider.get(BdmAccessControlApi.class);
-        BDMAccessControl bdmAccessControlStatus = accessControlApi.getBDMAccessControlStatus();
-        log.debug("BDM status: {}", bdmAccessControlStatus);
-        return bdmAccessControlStatus;
-    }
+	@Override
+	public BDMAccessControl getBdmAccessControlStatus() {
+		log.info("Get BDM AccessControl status");
+		if (isCommunity()) {
+			String message = "BDM AccessControl management skipped: your current Bonita license is 'Community' and this feature is only supported in 'Subscription' editions.";
+			throw new LicenseException(message);
+		}
+		BdmAccessControlApi accessControlApi = apiProvider.get(BdmAccessControlApi.class);
+		BDMAccessControl bdmAccessControlStatus = accessControlApi.getBDMAccessControlStatus();
+		log.debug("BDM status: {}", bdmAccessControlStatus);
+		return bdmAccessControlStatus;
+	}
+
+	public <T> List<T> query(String businessDataType, SearchBusinessDataQueryParams queryParams, Class<T> queryResultType) {
+		log.info("Query BDM for about {} objects with params {}", businessDataType, queryParams);
+		BusinessDataQueryApi queryApi = apiProvider.get(BusinessDataQueryApi.class);
+		List<Object> objects = queryApi.searchBusinessData(businessDataType, queryParams);
+
+		if (BusinessData.class.equals(queryResultType)) {
+			try {
+				return (List<T>) convertMany(objects.get(0), BusinessData.class);
+			}
+			catch (Exception e) {
+				log.error("Failed to parse response as BusinessData", e);
+				throw new ClientException("Failed to parse response as BusinessData", e);
+			}
+		}
+		else {
+			List<T> results = new ArrayList<>();
+			objects.forEach(o -> {
+				T result = convert(o, queryResultType);
+				results.add(result);
+			});
+			return results;
+		}
+	}
+
+	protected <T> List<T> convertMany(Object o, Class<T> queryResultType) {
+		try {
+			CollectionType collectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, queryResultType);
+			return (List<T>) objectMapper.readValue(o.toString(), collectionType);
+		}
+		catch (Exception e) {
+			log.error("Failed to parse response as BusinessData", e);
+			throw new ClientException("Failed to parse response as BusinessData", e);
+		}
+	}
+
+	protected <T> T convert(Object o, Class<T> queryResultType) {
+		try {
+			if (queryResultType.isInstance(o)) {
+				return queryResultType.cast(o);
+			}
+			if (Double.class.equals(queryResultType)) {
+				return (T) Double.valueOf(o.toString());
+			}
+			if (Long.class.equals(queryResultType)) {
+				return (T) Long.valueOf(o.toString());
+			}
+			if (Float.class.equals(queryResultType)) {
+				return (T) Float.valueOf(o.toString());
+			}
+			if (Integer.class.equals(queryResultType)) {
+				return (T) Integer.valueOf(o.toString());
+			}
+			throw new ClientException("Failed to convert query result from " + o.getClass() + " to " + queryResultType);
+		}
+		catch (ClassCastException | NumberFormatException e) {
+			throw new ClientException("Failed to convert query result from " + o.getClass() + " to " + queryResultType, e);
+		}
+	}
+
+	public <T> List<T> query(String businessDataType, String namedQuery, Class<T> queryResultType) {
+		log.info("Query BDM for about {} objects", businessDataType);
+		SearchBusinessDataQueryParams queryParams = new SearchBusinessDataQueryParams();
+		queryParams.q(namedQuery);
+		return query(businessDataType, queryParams, queryResultType);
+	}
 }
--- src/test/java/org/bonitasoft/web/client/services/impl/DefaultBdmServiceTest.java
+++ src/test/java/org/bonitasoft/web/client/services/impl/DefaultBdmServiceTest.java
@@ -1,17 +1,21 @@
 package org.bonitasoft.web.client.services.impl;
 
 import java.io.File;
+import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bonitasoft.web.client.BonitaClient;
 import org.bonitasoft.web.client.api.BdmAccessControlApi;
 import org.bonitasoft.web.client.api.BdmApi;
+import org.bonitasoft.web.client.api.BusinessDataQueryApi;
+import org.bonitasoft.web.client.api.BusinessDataQueryApi.SearchBusinessDataQueryParams;
 import org.bonitasoft.web.client.api.LicenseApi;
 import org.bonitasoft.web.client.api.SystemTenantApi;
 import org.bonitasoft.web.client.api.UploadApi;
 import org.bonitasoft.web.client.exception.LicenseException;
 import org.bonitasoft.web.client.feign.ApiProvider;
 import org.bonitasoft.web.client.model.BDMAccessControl;
+import org.bonitasoft.web.client.model.BusinessData;
 import org.bonitasoft.web.client.model.TenantPauseRequest;
 import org.bonitasoft.web.client.model.TenantResourceState;
 import org.bonitasoft.web.client.services.impl.base.CachingClientContext;
@@ -22,6 +26,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.bonitasoft.web.client.TestUtils.getClasspathFile;
 import static org.mockito.ArgumentMatchers.any;
@@ -34,107 +39,170 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 class DefaultBdmServiceTest {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private final ClientContext clientContext = new CachingClientContext();
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private DefaultBdmService bdmService;
+	private final ClientContext clientContext = new CachingClientContext();
 
-    @Mock
-    private ApiProvider apiProvider;
-    @Mock
-    private BdmApi bdmApi;
+	private DefaultBdmService bdmService;
+
 	@Mock
-    private UploadApi uploadApi;
-    @Mock
-    private SystemTenantApi tenantApi;
-    @Mock
-    private LicenseApi licenseApi;
-    @Mock
-    private BdmAccessControlApi accessControlApi;
+	private ApiProvider apiProvider;
 
-    @BeforeEach
-    void setUp() {
-        clientContext.clear();
-        bdmService = spy(new DefaultBdmService(clientContext, apiProvider, objectMapper));
+	@Mock
+	private BdmApi bdmApi;
 
-        lenient().when(apiProvider.get(LicenseApi.class)).thenReturn(licenseApi);
-        lenient().when(apiProvider.get(SystemTenantApi.class)).thenReturn(tenantApi);
-        lenient().when(apiProvider.get(BdmApi.class)).thenReturn(bdmApi);
-        lenient().when(apiProvider.get(UploadApi.class)).thenReturn(uploadApi);
-        lenient().when(apiProvider.get(BdmAccessControlApi.class)).thenReturn(accessControlApi);
+	@Mock
+	private BusinessDataQueryApi bdmQueryApi;
 
-        lenient().doReturn(true).when(bdmService).isCommunity();
-    }
+	@Mock
+	private UploadApi uploadApi;
 
-    @Test
-    void should_import_bdm() throws Exception {
-        // Given
-        File bdmFile = getClasspathFile("/bdm.zip");
+	@Mock
+	private SystemTenantApi tenantApi;
 
-        // When
-        bdmService.importBDM(bdmFile);
+	@Mock
+	private LicenseApi licenseApi;
 
-        // Then
-        // tenant paused
-        verify(tenantApi)
-                .updateSystemTenant(
-                        BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("true"));
-        verify(bdmService).deleteBdmAccessControlIfNeeded();
-        // bdm installed
-        verify(uploadApi).uploadFile(any());
-        verify(bdmApi).installBDM(any());
-        // tenant restarted
-        verify(tenantApi)
-                .updateSystemTenant(
-                        BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("false"));
-    }
+	@Mock
+	private BdmAccessControlApi accessControlApi;
 
-    @Test
-    void access_control_should_be_deleted_when_not_community_and_installed() {
-        // Given
-        doReturn(false).when(bdmService).isCommunity();
-        doReturn(new BDMAccessControl().state(TenantResourceState.INSTALLED))
-                .when(bdmService)
-                .getBdmAccessControlStatus();
+	@BeforeEach
+	void setUp() {
+		clientContext.clear();
+		bdmService = spy(new DefaultBdmService(clientContext, apiProvider, objectMapper));
 
-        // When
-        bdmService.deleteBdmAccessControlIfNeeded();
+		lenient().when(apiProvider.get(LicenseApi.class)).thenReturn(licenseApi);
+		lenient().when(apiProvider.get(SystemTenantApi.class)).thenReturn(tenantApi);
+		lenient().when(apiProvider.get(BdmApi.class)).thenReturn(bdmApi);
+		lenient().when(apiProvider.get(BusinessDataQueryApi.class)).thenReturn(bdmQueryApi);
+		lenient().when(apiProvider.get(UploadApi.class)).thenReturn(uploadApi);
+		lenient().when(apiProvider.get(BdmAccessControlApi.class)).thenReturn(accessControlApi);
 
-        // Then
-        verify(accessControlApi).deleteBDMAccessControl();
-    }
+		lenient().doReturn(true).when(bdmService).isCommunity();
+	}
 
-    @Test
-    void access_control_should_not_be_deleted_when_community() {
-        // Given
-        doReturn(true).when(bdmService).isCommunity();
+	@Test
+	void should_import_bdm() throws Exception {
+		// Given
+		File bdmFile = getClasspathFile("/bdm.zip");
 
-        // When
-        bdmService.deleteBdmAccessControlIfNeeded();
+		// When
+		bdmService.importBDM(bdmFile);
 
-        // Then
-        verify(accessControlApi, never()).deleteBDMAccessControl();
-    }
+		// Then
+		// tenant paused
+		verify(tenantApi)
+				.updateSystemTenant(
+						BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("true"));
+		verify(bdmService).deleteBdmAccessControlIfNeeded();
+		// bdm installed
+		verify(uploadApi).uploadFile(any());
+		verify(bdmApi).installBDM(any());
+		// tenant restarted
+		verify(tenantApi)
+				.updateSystemTenant(
+						BonitaClient.DEFAULT_TENANT_ID, new TenantPauseRequest().paused("false"));
+	}
 
-    @Test
-    void access_control_should_be_fetched_when_not_community() {
-        // Given
-        doReturn(false).when(bdmService).isCommunity();
+	@Test
+	void access_control_should_be_deleted_when_not_community_and_installed() {
+		// Given
+		doReturn(false).when(bdmService).isCommunity();
+		doReturn(new BDMAccessControl().state(TenantResourceState.INSTALLED))
+				.when(bdmService)
+				.getBdmAccessControlStatus();
 
-        // When
-        bdmService.getBdmAccessControlStatus();
+		// When
+		bdmService.deleteBdmAccessControlIfNeeded();
 
-        // Then
-        verify(accessControlApi).getBDMAccessControlStatus();
-    }
+		// Then
+		verify(accessControlApi).deleteBDMAccessControl();
+	}
 
-    @Test
-    void access_control_should_not_be_fetched_when_not_community() {
-        // Given
-        doReturn(true).when(bdmService).isCommunity();
+	@Test
+	void access_control_should_not_be_deleted_when_community() {
+		// Given
+		doReturn(true).when(bdmService).isCommunity();
 
-        // When
-        assertThatThrownBy(() -> bdmService.getBdmAccessControlStatus())
-                .isInstanceOf(LicenseException.class);
-    }
+		// When
+		bdmService.deleteBdmAccessControlIfNeeded();
+
+		// Then
+		verify(accessControlApi, never()).deleteBDMAccessControl();
+	}
+
+	@Test
+	void access_control_should_be_fetched_when_not_community() {
+		// Given
+		doReturn(false).when(bdmService).isCommunity();
+
+		// When
+		bdmService.getBdmAccessControlStatus();
+
+		// Then
+		verify(accessControlApi).getBDMAccessControlStatus();
+	}
+
+	@Test
+	void access_control_should_not_be_fetched_when_not_community() {
+		// Given
+		doReturn(true).when(bdmService).isCommunity();
+
+		// When
+		assertThatThrownBy(() -> bdmService.getBdmAccessControlStatus())
+				.isInstanceOf(LicenseException.class);
+	}
+
+	@Test
+	void bdm_query_should_return_type_data() {
+		// Given
+		SearchBusinessDataQueryParams queryParams = new SearchBusinessDataQueryParams();
+		queryParams.q("myNamedquery");
+		queryParams.p(0);
+		queryParams.c(Integer.MAX_VALUE);
+
+		// When
+		List<DummyBdmObject> queryResults = bdmService.queryList(DummyBdmObject.class.getName(), queryParams, DummyBdmObject.class);
+
+		// Then
+	}
+
+	@Test
+	void should_convert_from_string_to_double() {
+		Double value = bdmService.convert("1", Double.class);
+		assertThat(value).isInstanceOf(Double.class).isEqualTo(1);
+	}
+
+	@Test
+	void should_convert_from_string_to_float() {
+		Float value = bdmService.convert("1", Float.class);
+		assertThat(value).isInstanceOf(Float.class).isEqualTo(1);
+	}
+
+	@Test
+	void should_convert_from_string_to_int() {
+		Integer value = bdmService.convert("1", Integer.class);
+		assertThat(value).isInstanceOf(Integer.class).isEqualTo(1);
+	}
+
+	@Test
+	void should_convert_from_string_to_long() {
+		Long value = bdmService.convert("1", Long.class);
+		assertThat(value).isInstanceOf(Long.class).isEqualTo(1);
+	}
+
+	@Test
+	void should_convert_from_string_to_business_data() {
+		List<BusinessData> values = bdmService.convertMany("[{\"persistenceId_string\":\"123456\",\"persistenceVersion_string\":\"1\"}]", BusinessData.class);
+		assertThat(values)
+				.isNotEmpty()
+//				.isInstanceOf(BusinessData.class)
+//				.hasFieldOrPropertyWithValue("persistenceId_string", "123456")
+//				.hasFieldOrPropertyWithValue("persistenceVersion_string", "1")
+				;
+	}
+
+	public static final class DummyBdmObject {
+
+	}
 }
