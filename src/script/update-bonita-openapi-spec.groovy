7f6b60f6e28c9330596ca40aa1874d065f91dac8
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-05-11 12:04:12 +0200

    chore(action): fix typo

--- src/script/update-bonita-openapi-spec.groovy
+++ src/script/update-bonita-openapi-spec.groovy
@@ -7,7 +7,7 @@ def log = LoggerFactory.getLogger('')
 def basedir = new File(properties['basedir'])
 def bonitaSpecVersion = properties['bonita-openapi.version']
 def remoteBonitaSpec = "https://github.com/bonitasoft/bonita-openapi/releases/download/$bonitaSpecVersion/bonita-openapi-${bonitaSpecVersion}.yaml".toURL()
-def bonitaSpec = new File(basedir, 'api/bonita-openapi.yaml')
+def bonitaSpec = new File(basedir, 'api/openapi.yaml')
 log.info "Download spec from ${remoteBonitaSpec}..."
 bonitaSpec.write(remoteBonitaSpec.text)
-log.info "api/bonita-openapi.yaml updated with $bonitaSpecVersion content !"
+log.info "api/openapi.yaml updated with $bonitaSpecVersion content !"
